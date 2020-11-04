68aa9d8d02170942c00fc4a62fa7ca98dc6c0e90
Francisco Jr <miguelbh6@gmail.com>
2020-11-04 10:34:23 -0300

    :s parkles: desafio feito

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+
+    if (type !== 'outcome' && type !== 'income' ) {
+      throw new Error('This type not valid');
+    }
+
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error('You have not enough balance');
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
